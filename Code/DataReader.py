708ca24e49ee33b217bd05c050ae60209c17d449
Linzh <catmo1999@gmail.com>
2020-08-03 00:06:35 +0800

    update

--- Code/DataReader.py
+++ Code/DataReader.py
@@ -1,7 +1,7 @@
 import random
 
-class data:
-    def MyData(int rangeA = 0, int rangeB = 99, int k = 3, int b = 6):
-        data_x = list(range(rangeA, rangeB))
+class PrebuiltData:
+    def MyData(int rangeStart = 0, int rangeEnd = 99, rangeStep=1, int k = 3, int b = 6):
+        data_x = list(range(rangeStart, rangeEnd, rangeStep))
         data_y = list(map(lambda x: x * (k - 1 + random.random()) + b * random.random(), data_x))
         return data_x, data_y
