1a5cbc83d675fd3901bd90bb3b51f5442c32d0db
Linzh <catmo1999@gmail.com>
2020-08-02 10:26:35 +0800

    nya

--- Code/Cost.py
+++ Code/Cost.py
@@ -1,7 +1,11 @@
 import math
 
+
 class Cost:
     def LinerRegressionCost(data, predict, k, b):
+        if len(data) != len(predict):
+            print("Lengths of two data list are not equal.")
+            return 0x7fffffff
         bias = 0
         for index in range(0, len(data)):
             bias += ((data[index] - predict[index])**2) / 2
---
+++ Code/Enumerate.py
@@ -0,0 +1,19 @@
+import Cost
+
+
+class Enumerate:
+    def LinerRegressionEnumerate(data_x, data_y, maxRange=100, stepSize=1):
+        cost = 0x7fffffff
+        costSave = 0x7fffffff
+        kSave = 0
+        bSave = 0
+        for k in range(0, maxRange, stepSize):
+            for b in range(0, maxRange, stepSize):
+                predict_y = list(map(lambda x: x * k + b, data_x))
+                cost = Cost.LinerRegressionCost(data_y, predict_y, k, b)
+                if cost < costSave:
+                    kSave = k
+                    bSave = b
+                    costSave = cost
+                    print(k, b, cost)
+        return kSave, bSave, costSave
