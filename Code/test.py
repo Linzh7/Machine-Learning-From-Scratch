68ba985f57d3c751f96e9f31c6ed2f3ca403e0b5
Linzh <catmo1999@gmail.com>
2020-08-11 20:42:52 +0800

    Update

--- Code/test.py
+++ Code/test.py
@@ -4,9 +4,11 @@ import DataReader
 
 
 class GradientDescent():
-    def GradientDescent(data_x, data_y, order, alpha=0.001, error=0.001):
+    def GradientDescent(data_x, data_y, order, error=1, alpha=0.001):
         count = 0
         order += 1
+        theta = []
+        diff = []
         for i in range(order):
             theta.append(np.random.randint(100))
             diff.append(0)
@@ -21,36 +23,15 @@ class GradientDescent():
                             diff[i] += (theta[j] * data_x[index]**i)
                 for i in range(order):
                     theta[i] -= alpha * diff[i]
-                predict_y = np.array(theta0 + theta1 * data_x)
+                predict_y = np.zeros(data_x.shape)
+                for i in range(order):
+                    predict_y += theta[i] * data_x**i
                 predictCost = Cost.SquaredErrors(data_x, predict_y)
                 if count % 100 == 0:
                     print(count, predictCost)
         return theta, predictCost
 
 
-def LinerLeastSquaresMethod(data_x, data_y):
-    xMean = np.mean(data_x)
-    sumYX = 0
-    sumX2 = 0
-    for i in np.arange(len(data_x)):
-        x = data_x[i]
-        y = data_y[i]
-        sumYX += y * (x - xMean)
-        sumX2 += x**2
-    k = sumYX / (sumX2 - len(data_x) * (xMean**2))
-    sumDelta = 0
-    for i in np.arange(len(data_x)):
-        x = data_x[i]
-        y = data_y[i]
-        sumDelta += (y - k * x)
-    b = sumDelta / len(data_x)
-    return k, b
-
-
 data_x, data_y = DataReader.PrebuiltData.MyGivenData()
-k, b = LinerLeastSquaresMethod(data_x, data_y)
-predict_y = np.array(data_x * k + b)
-print(k, b)
-print(Cost.SquaredErrors(data_y, predict_y))
-'''theta, cost = GradientDescent.LinerGradientDescent(data_x, data_y)
-print(theta, cost)'''
+theta, cost = GradientDescent.GradientDescent(data_x, data_y, 1)
+print(theta, cost)
