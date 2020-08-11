f91ff73914050deb3f6ce73dae7eea344de69d52
Linzh <catmo1999@gmail.com>
2020-08-11 20:28:58 +0800

    Update least squares method

--- Code/test.py
+++ Code/test.py
@@ -4,20 +4,22 @@ import DataReader
 
 
 class GradientDescent():
-    def LinerGradientDescent(data_x, data_y, alpha=0.001, error=0.001):
+    def GradientDescent(data_x, data_y, order, alpha=0.001, error=0.001):
         count = 0
-        theta = [np.random.randint(100), np.random.randint(100)]
-        diff = [0, 0]
+        order += 1
+        for i in range(order):
+            theta.append(np.random.randint(100))
+            diff.append(0)
         dataCost = Cost.SquaredErrors(data_x, data_y)
         predictCost = 0x7fffffff
         while predictCost - dataCost > error:
             count += 1
             for index in range(len(data_x)):
-                for i in range(len(theta)):
-                    for j in range(len(theta)):
+                for i in range(order):
+                    for j in range(order):
                         if data_x[index] != 0 or i != 0:
                             diff[i] += (theta[j] * data_x[index]**i)
-                for i in range(len(theta)):
+                for i in range(order):
                     theta[i] -= alpha * diff[i]
                 predict_y = np.array(theta0 + theta1 * data_x)
                 predictCost = Cost.SquaredErrors(data_x, predict_y)
@@ -26,24 +28,29 @@ class GradientDescent():
         return theta, predictCost
 
 
-class Enumerate():
-    def LinerRegressionEnumerate(data_x, data_y, maxRange=10, stepSize=0.1):
-        cost = costSave = 0x7fffffff
-        kSave = 0
-        bSave = 0
-        for k in np.arange(0, maxRange, stepSize):
-            for b in np.arange(0, maxRange, stepSize):
-                predict_y = np.array(data_x * k + b)
-                cost = Cost.SquaredErrors(data_y, predict_y)
-                if cost < costSave:
-                    kSave = k
-                    bSave = b
-                    costSave = cost
-                    #print(k, b, cost)
-        return kSave, bSave, costSave
+def LinerLeastSquaresMethod(data_x, data_y):
+    xMean = np.mean(data_x)
+    sumYX = 0
+    sumX2 = 0
+    for i in np.arange(len(data_x)):
+        x = data_x[i]
+        y = data_y[i]
+        sumYX += y * (x - xMean)
+        sumX2 += x**2
+    k = sumYX / (sumX2 - len(data_x) * (xMean**2))
+    sumDelta = 0
+    for i in np.arange(len(data_x)):
+        x = data_x[i]
+        y = data_y[i]
+        sumDelta += (y - k * x)
+    b = sumDelta / len(data_x)
+    return k, b
 
 
 data_x, data_y = DataReader.PrebuiltData.MyGivenData()
-k, b, cost = Enumerate.LinerRegressionEnumerate(data_x, data_y, 3.1, 0.005)
+k, b = LinerLeastSquaresMethod(data_x, data_y)
+predict_y = np.array(data_x * k + b)
+print(k, b)
+print(Cost.SquaredErrors(data_y, predict_y))
 '''theta, cost = GradientDescent.LinerGradientDescent(data_x, data_y)
 print(theta, cost)'''
