b25eed278f7959e37fb0af7f5b02731669379253
Linzh <catmo1999@gmail.com>
2020-08-13 20:15:35 +0800

    Update

--- Code/test.py
+++ Code/test.py
@@ -4,32 +4,53 @@ import DataReader
 
 
 class GradientDescent():
-    def GradientDescent(data_x, data_y, order, error=1, alpha=0.001):
+    def LinerGradientDescent(data_x, data_y, epsilon=0.01, alpha=0.001):
         count = 0
-        order += 1
-        theta = []
+        theta = [0, 0]
         diff = 0
-        for i in range(order):
-            theta.append(0)
-        dataCost = Cost.SquaredErrors(data_x, data_y)
-        predictCost = 0x7fffffff
-        while predictCost - dataCost > error:
+        while True:
+            count += 1
+            for index in range(len(data_x)):
+                diff = theta[0] + theta[1] * data_x[index] - data_y[index]
+
+                theta[0] -= alpha * diff
+                theta[1] -= alpha * diff * data_x[index]
+
+                predict_y = theta[0] + theta[1] * data_x
+
+                predictCost0 = Cost.SquaredErrors(data_x, predict_y)
+
+                if count % 100 == 0:
+                    print(count, predictCost0)
+
+        if predictCost0 - predictCost1 < epsilon:
+            return theta, predictCost
+        predictCost1 = predictCost0
+
+    def G1radientDescent(data_x, data_y, epsilon=0.001, alpha=0.001):
+        count = 0
+        theta = [0, 0]
+        diff = 0
+        while True:
             count += 1
             for index in range(len(data_x)):
                 diff = 0
                 for i in range(order):
-                    diff += theta[i] * data_x[index]**i
+                    if data_x[index] != 0 or i != 0:
+                        diff += theta[i] * data_x[index]**i - data_y[index]
                 for i in range(order):
                     theta[i] -= alpha * diff * data_x[index]
                 predict_y = np.zeros(data_x.shape)
                 for i in range(order):
                     predict_y += theta[i] * data_x**i
-                predictCost = Cost.SquaredErrors(data_x, predict_y)
+                predictCost0 = Cost.SquaredErrors(data_x, predict_y)
                 if count % 100 == 0:
                     print(count, predictCost)
-        return theta, predictCost
+        if predictCost0 - predictCost1 < epsilon:
+            return theta, predictCost
+        predictCost1 = predictCost0
 
 
 data_x, data_y = DataReader.PrebuiltData.MyGivenData()
-theta, cost = GradientDescent.GradientDescent(data_x, data_y, 1)
+theta, cost = GradientDescent.LinerGradientDescent(data_x, data_y)
 print(theta, cost)
