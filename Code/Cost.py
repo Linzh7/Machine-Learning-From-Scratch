ffb12d00dd09415a94f6d977188e385b13e9047c
Linzh <catmo1999@gmail.com>
2020-08-30 18:31:12 +0800

    Update distance

--- Code/Cost.py
+++ Code/Cost.py
@@ -13,3 +13,24 @@ class Cost():
             bias += ((data[index] - predict[index])**2) / 2
         bias /= 2
         return bias
+
+
+class distance():
+    def Euclidean(a, b):
+        sum = 0
+        for i in len(a):
+            sum += (a[i] - b[i])**2
+        return math.sqrt(sum)
+
+    def Manhattan(a, b):
+        sum = 0
+        for i in len(a):
+            sum += abs(a[i] - b[i])
+        return sum
+
+    def Minkowski(a, b, p):
+        sum = 0
+        p = len(a)
+        for i in p:
+            sum += (abs(a[i] - b[i]))**p
+        return sum**(1 / p)
