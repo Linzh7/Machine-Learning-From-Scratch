9cc552bc288b275e92241d57bd2f8471e78b702b
Linzh <catmo1999@gmail.com>
2020-09-09 13:49:45 +0800

    Update

--- Code/Cost.py
+++ Code/Cost.py
@@ -2,35 +2,35 @@ import numpy as np
 import math
 
 
-class Cost():
-    def SquaredErrors(data, predict):
-        # detect length
-        if data.shape != predict.shape:
-            print("Lengths of two data list are not equal.")
-            return -1
-        bias = 0
-        for index in range(0, predict.shape[0]):
-            bias += ((data[index] - predict[index])**2) / 2
-        bias /= 2
-        return bias
+def SquaredErrors(data, predict):
+    # detect length
+    if data.shape != predict.shape:
+        print("Lengths of two data list are not equal.")
+        return -1
+    bias = 0
+    for index in range(0, predict.shape[0]):
+        bias += ((data[index] - predict[index])**2) / 2
+    bias /= 2
+    return bias
 
 
-class Distance():
-    def Euclidean(a, b):
-        sum = 0
-        for i in range(len(a)):
-            sum += (a[i] - b[i])**2
-        return math.sqrt(sum)
+def Euclidean(a, b):
+    sum = 0
+    for i in range(len(a)):
+        sum += (a[i] - b[i])**2
+    return math.sqrt(sum)
 
-    def Manhattan(a, b):
-        sum = 0
-        for i in range(len(a)):
-            sum += abs(a[i] - b[i])
-        return sum
 
-    def Minkowski(a, b, p):
-        sum = 0
-        p = len(a)
-        for i in range(p):
-            sum += (abs(a[i] - b[i]))**p
-        return sum**(1 / p)
+def Manhattan(a, b):
+    sum = 0
+    for i in range(len(a)):
+        sum += abs(a[i] - b[i])
+    return sum
+
+
+def Minkowski(a, b, p):
+    sum = 0
+    p = len(a)
+    for i in range(p):
+        sum += (abs(a[i] - b[i]))**p
+    return sum**(1 / p)
--- Code/DataOperation.py
+++ Code/DataOperation.py
@@ -1,26 +1,25 @@
-class PreOperation():
-    def FillMissingData(self, sets):
-        for index in range(sets.shape[0]):
-            if sets[index] == -1:
-                sets[index] = sets.mean()
-        return sets
-
-    def LablePressing(self, data):
-        dic = {}
-        for lable in data:
-            if lable in dic:
-                dic[lable] += 1
-            else:
-                dic[lable] = 1
-        mapping = []
-        count = 1
-        for k, v in dic.items():
-            mapping.append([count, k])
-        return mapping
+def FillMissingData(self, sets):
+    for index in range(sets.shape[0]):
+        if sets[index] == -1:
+            sets[index] = sets.mean()
+    return sets
 
 
-class xxx:
-    def Normalization(self, sets):
-        for i in range(len(sets[0])):
-            sets[:, i] = (sets[:, i] - sets[:, i].min()) / sets[:, i].max()
-        return sets
+def LablePressing(self, data):
+    dic = {}
+    for lable in data:
+        if lable in dic:
+            dic[lable] += 1
+        else:
+            dic[lable] = 1
+    mapping = []
+    count = 1
+    for k, v in dic.items():
+        mapping.append([count, k])
+    return mapping
+
+
+def Normalization(self, sets):
+    for i in range(len(sets[0])):
+        sets[:, i] = (sets[:, i] - sets[:, i].min()) / sets[:, i].max()
+    return sets
